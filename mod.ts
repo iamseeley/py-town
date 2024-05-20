a8f8990bfa045d5f50be8d164219b778837c8858
iamseeley <seeleypersonal@gmail.com>
2024-05-20 08:31:21 -0400

    adding runPythonCodeInWorker - getting off the main thread

--- mod.ts
+++ mod.ts
@@ -1,28 +1,32 @@
 import { initializePyodide } from "./pyodide_setup.ts";
 import { executePython } from "./python_executor.ts";
 
-
 export async function runPythonCode(code: string) {
     const pyodide = await initializePyodide();
     return await executePython(pyodide, code);
 }
 
+export function runPythonCodeInWorker(code: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(new URL("./pyodide-worker.ts", import.meta.url), { type: "module" });
 
-export async function runPythonCodeInWorker(code: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const worker = new Worker(new URL('./pyodide-worker.ts', import.meta.url).href, { type: 'module' });
+        worker.onmessage = (event) => {
+            const { result, error } = event.data;
+            worker.terminate();
+            if (error) {
+                reject(error);
+            } else {
+                resolve(result);
+            }
+        };
 
-    worker.onmessage = (event) => {
-      const { result, error } = event.data;
-      worker.terminate();
-      if (result) {
-        resolve(result);
-      } else {
-        reject(error);
-      }
-    };
+        worker.onerror = (error) => {
+            worker.terminate();
+            reject(error.message);
+        };
 
-    worker.postMessage({ code });
-  });
+        worker.postMessage({ code });
+    });
 }
+
 
--- pyodide-worker.ts
+++ pyodide-worker.ts
@@ -1,21 +1,20 @@
 import { initializePyodide } from "./pyodide_setup.ts";
 import { executePython } from "./python_executor.ts";
 
-let pyodide;
-
-async function loadPyodideAndPackages() {
-  pyodide = await initializePyodide();
-}
-
-loadPyodideAndPackages();
+let pyodide: any;
 
 self.onmessage = async (event) => {
-  const { code } = event.data;
-  try {
-    const result = await executePython(pyodide, code);
-    self.postMessage({ result });
-  } catch (error) {
-    self.postMessage({ error: error.message });
-  }
+    const { code } = event.data;
+
+    if (!pyodide) {
+        pyodide = await initializePyodide();
+    }
+
+    try {
+        const result = await executePython(pyodide, code);
+        self.postMessage({ result });
+    } catch (error) {
+        self.postMessage({ error: error.message });
+    }
 };
 
